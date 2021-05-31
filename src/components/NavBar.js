56b646514b25352ddf5428681bcc0e1e01e4041d
patoGarci <garcia.pato1985@gmail.com>
2021-05-31 21:40:42 +0200

    add navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './NavBar.css'
+export default function NavBar() {
+    return (
+        <>
+            <div className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Ecommerce</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Categoy
+                            </a>
+                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a className="dropdown-item" href="#">CelPhones</a></li>
+                                <li><a className="dropdown-item" href="#">TV</a></li>
+                                <li><hr className="dropdown-divider"/></li>
+                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                            </li>
+                            <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Contact</a>
+                            </li>
+                            {/* <li className="nav-item">
+                            <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                            </li> */}
+                        </ul>
+                        {/* <form className="d-flex">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form> */}
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
