ee292693cc236cb2960680295e1c0adb9237b4e1
patoGarci <garcia.pato1985@gmail.com>
2021-06-03 04:46:26 +0200

    contador

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import ItemCount from '../ItemCount/ItemCount'
 export default function ItemListContainer() {
+    const onAdd = (e,count)=>{
+        alert(`agregaste ${count} al carrito`);
+        count = 1;
+    }
     return (
-        <div>
-            Catalogo
-        </div>
+        <>
+            <ItemCount stock={5} initial={1} onAdd={onAdd}/>
+        </>
     )
 }
